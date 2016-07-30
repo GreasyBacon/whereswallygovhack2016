02b06d09f4dfbb8ed5d13ee294c1e75710da2864
Rhys Walsh-Tindall <rhys_walsh@hotmail.com>
2016-07-30 12:19:44 +1000

    re-writing de-identified spreadsheet dataset

--- routes/visualise.js
+++ routes/visualise.js
@@ -15,7 +15,7 @@ router.post('/', function(req, res) {
   	var firstSheetName = workbook.SheetNames[0];
   	var worksheet = XLSX.utils.sheet_to_json(workbook.Sheets[firstSheetName]);
 	
-  	//var outputWorkbook = new Workbook();
+  	//var outputWorkbook = new XLSX.Workbook();
   	//var outputFilename = '/uploads/output-' + req.body.filename
   	//outputWorkbook.SheetNames.push("output");
   	//define ws as written sheet
@@ -75,17 +75,37 @@ router.post('/', function(req, res) {
 
   		for (var rowIndex in worksheet) {
   			var value = Number(worksheet[rowIndex][column.column]);
-  			values += value;
-  			count += 1;  			
+  			if (!isNaN(value)){
+  				values += value;
+  				count += 1;
+  			}  			
   		}
 
-  		var average = values / count;
+  		var average = Math.round(values / count);
 
   		addToOutput(column.column, 'average', average);
 
   	};
 
   	var visualiseRange = function(index, column) {
+
+  		var max = 0,
+  			min,
+  			range;
+  		
+  		for (var rowIndex in worksheet) {
+  			var value = Number(worksheet[rowIndex][column.column]);
+  			if (!min < value) {
+  				min = value;
+  			}
+  			if (value > max) {
+  				max = value;
+  			}
+  		}
+
+  		range = max - min;
+
+  		addToOutput(column.column, 'range', [min, max, range])
 
   	};
 
@@ -119,6 +139,65 @@ router.post('/', function(req, res) {
 
   	};
 
+  	var generateXLSXFile = function(data, filename) {
+
+	  	/*https://gist.github.com/SheetJSDev/88a3ca3533adf389d13c*/
+	  	function datenum(v, date1904) {
+			if(date1904) v+=1462;
+			var epoch = Date.parse(v);
+			return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
+		}	
+
+		function sheet_from_array_of_arrays(data, opts) {
+			var ws = {};
+			var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
+			for(var R = 0; R != data.length; ++R) {
+				for(var C = 0; C != data[R].length; ++C) {
+					if(range.s.r > R) range.s.r = R;
+					if(range.s.c > C) range.s.c = C;
+					if(range.e.r < R) range.e.r = R;
+					if(range.e.c < C) range.e.c = C;
+					var cell = {v: data[R][C] };
+					if(cell.v == null) continue;
+					var cell_ref = XLSX.utils.encode_cell({c:C,r:R});
+					
+					if(typeof cell.v === 'number') cell.t = 'n';
+					else if(typeof cell.v === 'boolean') cell.t = 'b';
+					else if(cell.v instanceof Date) {
+						cell.t = 'n'; cell.z = XLSX.SSF._table[14];
+						cell.v = datenum(cell.v);
+					}
+					else cell.t = 's';
+					
+					ws[cell_ref] = cell;
+				}
+			}
+			if(range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
+			return ws;
+		}
+
+		/* original data */
+		var ws_name = "De-Identified Data";
+
+		function Workbook() {
+			if(!(this instanceof Workbook)) return new Workbook();
+			this.SheetNames = [];
+			this.Sheets = {};
+		}
+
+		var wb = new Workbook(), ws = sheet_from_array_of_arrays(data);
+
+		wb.SheetNames.push(ws_name);
+		wb.Sheets[ws_name] = ws;
+		XLSX.writeFile(wb, 'uploads/' + filename);
+		/*https://gist.github.com/SheetJSDev/88a3ca3533adf389d13c*/
+  	}
+
+  	var allColumnHeaders = [];
+  	var columnsToKeep = [];
+  	var columnsToPseudonymise = [];
+  	var spreadsheetArray = [];
+
   	for (var i=0; i<columns.length; i++) {
 
   		var column = columns[i]
@@ -146,36 +225,53 @@ router.post('/', function(req, res) {
   				break
   		}
 
-  		switch(columns[i].remove) {
+  		switch(column.remove) {
   			case true:
+  				//not going to be used
   				break
   			case false:
-  				break
-  		}
-
-  		switch(columns[i].pseudonymise) {
-  			case true:
-  				break
-  			case false:
+  				if (column.pseudonymise) {
+  					columnsToPseudonymise.push(column.column);
+  				} else {
+  					columnsToKeep.push(column.column);
+  				}
+  				allColumnHeaders.push(column.column);
   				break
   		}
 
   	};
 
-  	//does it need to be added to new spreadsheet
-  	//does it need to be psuedoified?
-  	//does it need to be visualised
+  	//get headers in spreadsheet
+  	spreadsheetArray.push(allColumnHeaders);
 
-  	//count
-  	//count unique
-  	//average
-  	//range
-  	//max
-  	//min
+  	for (var rowIndex in worksheet) {
+
+		var cellObject = worksheet[rowIndex];
+		var cellValues = [];
+
+		for (var i=0; i<allColumnHeaders.length; i++) {
+			var column = allColumnHeaders[i];
+			var val = cellObject[column] || ''; //account for blank cells
+			if (columnsToKeep.indexOf(column) == -1 && columnsToPseudonymise.indexOf(column) == -1) {
+				delete val;
+			} else if (columnsToPseudonymise.indexOf(column) !== -1) {
+				val = "SHOULD BE HASHED";
+				cellValues.push(val);
+			} else {
+				cellValues.push(val);
+			}
+		}
+
+		spreadsheetArray.push(cellValues);
+
+	}
+
+	generateXLSXFile(spreadsheetArray, 'de-identified-' + req.body.filename)
 
 	res.json({
         'success': true,
         'output': output,
+        'file': 'de-identified' + req.body.filename,
         'statusMsg': "Processing completed."
      });
 
